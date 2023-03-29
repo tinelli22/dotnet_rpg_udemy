0cc59f35ef32b7c66efd4b8f5641ab3b8ec862f9
Michael <michaelrtinelli@gmail.com>
2023-03-29 16:39:58 -0300

    initializing on integrate with sql db docker container

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg_udemy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg_udemy.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+        
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using dotnet_rpg_udemy.Data;
 using dotnet_rpg_udemy.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
